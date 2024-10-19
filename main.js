8711a837b8a4c1bc3dcbf5f49bb8433e61b3872e
Mehul Rana <mehulrana412@gmail.com>
2024-10-19 18:01:15 -0500

    updated stage names so it's easier to use with JavaScript

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector('#kahnsarena').addEventListener('click', kahnsarena)
+
+function kahnsarena(){
+  document.querySelector('body').style.backgroundImage = "url(./stages/kahnsarena.png)"
+}
