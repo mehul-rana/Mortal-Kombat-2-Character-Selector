0fbcc57033a5efc688ea2c54c4285ffa818d4bed
Mehul Rana <mehulrana412@gmail.com>
2024-10-19 18:49:36 -0500

    started JavaScript

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ document.querySelector('#kahnsarena').addEventListener('click', kahnsarena)
 function kahnsarena(){
   document.querySelector('body').style.backgroundImage = "url(./stages/kahnsarena.png)"
 }
+
+document.querySelector('#lk1').addEventListener('click', lk1)
+
+function lk1(){
+  // !toggle hidden class
+}
